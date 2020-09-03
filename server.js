18c954fb62bce603aee4fef27bddb683a8ec6cca
Simon Xu <simon@liyaintl.com>
2020-09-02 22:59:57 -0600

    figured out using mongoDB atlas, set up MONGODB_URI

--- server.js
+++ server.js
@@ -15,10 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// use mongoDB local host
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+// use mongoDB atlas online
+mongoose.connect(process.env.MONGODB_URI)
 
 // routes
 app.use(require("./routes/api.js"));
